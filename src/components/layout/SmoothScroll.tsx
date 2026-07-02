17bcefc2fb9b31700762d8cbd486c1d84a26394c
Neelapu Nani <neelapunani2580@gmail.com>
2026-07-02 11:22:28 +0530

    Pushed the ui chaneg

--- src/components/layout/SmoothScroll.tsx
+++ src/components/layout/SmoothScroll.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { ReactLenis } from '@studio-freight/react-lenis'
-
+import { ReactLenis } from 'lenis/react'
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
     <ReactLenis root options={{ lerp: 0.08, duration: 1.5, smoothWheel: true }}>
